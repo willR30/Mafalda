c4e87f0093119706e22744bc685b46f6e0b39aa1
william <prueba@correo.com>
2020-11-18 12:51:55 -0600

    Punto de partida hasta las aplicaciones, faltan detalles de interfaz y validacion de bugs

---
+++ src/calculo/Calculo.java
@@ -0,0 +1,17 @@
+
+package calculo;
+
+
+public class Calculo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**/
+        Home home =new Home();
+        home.setVisible(true);
+    }
+    
+}
